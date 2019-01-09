7de34f42571e1248d84a8892681dbf8b7a8453bd
balkanskiy <balkanskiy.alexey@gmail.com>
2019-01-09 21:26:18 +0300

    Add basic layouts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import css from "./styles/app.module.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className={css.layout}>
+        <header className={css.header}>
+          <b className={css.title}>IntelliJ IDEA 2017.3! Help</b>
         </header>
+        <div className={css.app}>
+          <div className={css.nav}>nav</div>
+          <div className={css.content}>
+            Morbi malesuada dui dui, ut lacinia elit malesuada id. Quisque eu
+            leo faucibus, posuere dui sit amet, aliquet tortor. Suspendisse
+            mauris lacus, rhoncus quis sollicitudin in, bibendum a quam. Donec
+            cursus urna risus, sit amet faucibus lacus facilisis at. Phasellus
+            mollis tempor velit ac finibus. Morbi volutpat porttitor dolor vitae
+            finibus. Proin pellentesque dignissim metus, non rutrum arcu blandit
+            vitae. Sed commodo mauris et dignissim euismod. In pulvinar sodales
+            purus. Nulla mollis elit urna, lacinia tincidunt mi aliquam id. Sed
+            vel ex facilisis, placerat orci id, pulvinar odio. Maecenas
+            fermentum massa quis libero hendrerit commodo. Proin nisl sapien,
+            aliquam at leo id, vulputate varius lectus. Vivamus at egestas urna.
+          </div>
+        </div>
       </div>
     );
   }
