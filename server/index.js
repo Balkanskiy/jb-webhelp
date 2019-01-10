0485e2548bb82feeaf199ccd9b8f4f945e534840
a.balkanskiy <a.balkanskiy@securitycode.ru>
2019-01-10 11:42:13 +0300

    Add web server

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const data = require("./data.json");
+
+const app = express();
+const port = 8080;
+
+app.get("/", (request, response) => {
+  response.json(data);
+});
+
+app.listen(port, err => {
+  if (err) {
+    return console.log("something bad happened", err);
+  }
+
+  console.log(`server is listening on ${port}`);
+});
