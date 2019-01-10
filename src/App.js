3876f3ed08f20f56afc6a6fc43d2ea5ca1b3f1ed
balkanskiy <balkanskiy.alexey@gmail.com>
2019-01-10 23:19:47 +0300

    Rename styles file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Index from "./components/SideMenu/";
-import css from "./styles.module.css";
+import css from "./index.module.css";
 
 class App extends Component {
   render() {
