c428e2765842c35d58981772e5648db792b05e32
a.balkanskiy <a.balkanskiy@securitycode.ru>
2019-01-14 11:28:47 +0300

    Return server

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const data = require("./data.json");
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.get("/help/idea", (request, response) => {
+  setTimeout(() => {
+    response.json(data);
+  }, 3000);
+});
+
+app.listen(port, err => {
+  if (err) {
+    return console.log("something bad happened", err);
+  }
+
+  console.log(`server is listening on ${port}`);
+});
